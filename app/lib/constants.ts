b02d59ba7b35e8f671ae9257ed510d3313be4f62
BestVersion7 <hunterkfan@gmail.com>
2025-01-25 10:02:04 -0600

    update baseurl

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,7 +1,7 @@
 export let BASE_URL: string;
 
 if (process.env.NODE_ENV == "production") {
-    BASE_URL = "https://afashionstore.vercel.app";
+    BASE_URL = "https://babywishlist.vercel.app";
 } else {
     BASE_URL = "http://localhost:3000";
 }
