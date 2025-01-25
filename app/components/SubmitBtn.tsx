4c89739c61d77f6b019eb72327942002fac44550
BestVersion7 <hunterkfan@gmail.com>
2025-01-25 10:14:58 -0600

    move suspense to home

--- app/components/SubmitBtn.tsx
+++ app/components/SubmitBtn.tsx
@@ -4,7 +4,6 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { Modal } from "./Modal";
 import { PurchaseForm } from "./PurchaseForm";
 import { RegaloType } from "../types";
-import { Suspense } from "react";
 
 export const SubmitBtn = (props: {
     selectedItems: Pick<RegaloType, "id" | "item">[];
@@ -18,23 +17,21 @@ export const SubmitBtn = (props: {
     const modal = useSearchParams().get("modal");
     return (
         <>
-            <Suspense fallback={<div>Loading...</div>}>
-                {modal === "t" && (
-                    <Modal>
-                        <PurchaseForm selectedItems={props.selectedItems} />
-                    </Modal>
-                )}
+            {modal === "t" && (
+                <Modal>
+                    <PurchaseForm selectedItems={props.selectedItems} />
+                </Modal>
+            )}
 
-                <button
-                    className="mt-4 w-full bg-coffee-600 text-white py-2 px-4 rounded hover:bg-coffee-500 transition duration-200"
-                    // className="z-20  flex items-center gap-2 text-xl p-4 bg-yellow-300 text-blue-800 font-bold rounded-xl hover:bg-yellow-400 hover:cursor-pointer"
-                    type="button"
-                    onClick={handleOpen}
-                    // disabled={props.selectedItems.length < 1}
-                >
-                    Purchase
-                </button>
-            </Suspense>
+            <button
+                className="mt-4 w-full bg-coffee-600 text-white py-2 px-4 rounded hover:bg-coffee-500 transition duration-200"
+                // className="z-20  flex items-center gap-2 text-xl p-4 bg-yellow-300 text-blue-800 font-bold rounded-xl hover:bg-yellow-400 hover:cursor-pointer"
+                type="button"
+                onClick={handleOpen}
+                // disabled={props.selectedItems.length < 1}
+            >
+                Purchase
+            </button>
         </>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { ProductForm } from "./components/ProductForm";
 import { getRegalos } from "./utils/apiCalls";
+import { Suspense } from "react";
 
 export default async function Home() {
     const regalos = await getRegalos();
     return (
         <main>
-            <ProductForm regalos={regalos} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <ProductForm regalos={regalos} />
+            </Suspense>
         </main>
     );
 }
